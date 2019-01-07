57d9b21e09bf1918d3b22b305ec049c789d9b3ae
hisaki-kanno <hisaki.kanno@litalico.co.jp>
2019-01-07 22:41:41 +0900

    ゲームパッドの切断に対応した

--- index.js
+++ index.js
@@ -11,11 +11,26 @@ window.addEventListener("gamepadconnected", function (e) {
     e.gamepad.buttons.length, e.gamepad.axes.length);
 });
 
+window.addEventListener("gamepaddisconnected", function (e) {
+  var i = getPointersIndex(e.gamepad.index);
+  pointers.splice(i, 1);
+});
+
+var getPointersIndex = function (gamepadIndex) {
+  for (var k = 0, l = pointers.length; k < l; k++) {
+    if (pointers[k].gamepadIndex == gamepadIndex) {
+      return k;
+    }
+  }
+};
+
 var render = function () {
   context.clearRect(0, 0, 3000, 1900);
 
   pointers.forEach(function (pointer) {
-    pointer.render(context);
+    if (pointer) {
+      pointer.render(context);
+    }
   });
 
   requestAnimationFrame(render);
